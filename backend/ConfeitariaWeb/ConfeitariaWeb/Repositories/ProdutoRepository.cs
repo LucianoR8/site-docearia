6f1d82d90035a38803e3ec898e957ffcb223e58f
LucianoR8 <luciribsantos@gmail.com>
2026-07-20 23:20:48 -0300

    Implementação de 3 métodos de consulta aos dados
    ObterTodos traz todos os produtos cadastrados no banco de dados, ObterPorId traz um produto específico, e ObterPorCategoria traz os produtos relacionados a uma categoria específica.

--- backend/ConfeitariaWeb/ConfeitariaWeb/Repositories/ProdutoRepository.cs
+++ backend/ConfeitariaWeb/ConfeitariaWeb/Repositories/ProdutoRepository.cs
@@ -7,9 +7,25 @@ namespace ConfeitariaWeb.Repositories
 {
     public class ProdutoRepository  : IProdutoRepository
     {
+        private readonly AppDbContext _context;
+
+        public ProdutoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
         public async Task<List<Produto>> ObterTodosAsync()
         {
+            return await _context.Produtos.Include(p => p.Categoria).OrderBy(p => p.NomeProduto).ToListAsync();
+        }
 
+        public async Task<Produto?> ObterPorIdAsync(int id)
+        {
+            return await _context.Produtos.Include(p => p.Categoria).FirstOrDefaultAsync(p => p.IdProduto == id);
+        }
+
+        public async Task<List<Produto>> ObterPorCategoriaAsync(int categoriaId)
+        {
+            return await _context.Produtos.Include(p => p.Categoria).Where(p => p.CategoriaId == categoriaId).OrderBy(p => p.NomeProduto).ToListAsync();
         }
     }
 }
